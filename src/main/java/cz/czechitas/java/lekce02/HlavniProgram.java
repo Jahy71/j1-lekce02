9e846f7a75480ecbf2e9ff794b6c998baebd14b6
Jahy71 <jiri.jahoda@email.cz>
2023-02-14 20:42:40 +0100

    ukol

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -7,6 +7,72 @@ public class HlavniProgram {
     public void start() {
         zofka = new Turtle();
 
+        zofka.turnRight(180);
+
+        for (int i = 0; i < 4; i++) {
+            zofka.move(150);
+            zofka.turnLeft(90);
+        }
+
+        zofka.turnLeft(135);
+
+        for (int i = 0; i < 2; i++) {
+            zofka.move(105);
+            zofka.turnRight(90);
+        }
+
+        zofka.turnRight(45);
+        zofka.penUp();
+        zofka.move(350);
+        zofka.turnLeft(90);
+
+        zofka.penDown();
+
+        for (int i = 0; i < 4; i++) {
+            zofka.move(150);
+            zofka.turnLeft(90);
+        }
+
+        zofka.turnLeft(135);
+
+        for (int i = 0; i < 2; i++) {
+            zofka.move(105);
+            zofka.turnRight(90);
+        }
+
+        zofka.turnRight(225);
+        zofka.penUp();
+        zofka.move(350);
+        zofka.penDown();
+
+        for (int i = 0; i < 2; i++) {
+            zofka.move(200);
+            zofka.turnRight(90);
+            zofka.move(150);
+            zofka.turnRight(90);
+        }
+
+        zofka.turnRight(135);
+        zofka.move(106);
+        zofka.turnLeft(90);
+        zofka.move(106);
+        zofka.turnLeft(45);
+        zofka.move(50);
+
+        for (int i = 0; i < 2; i++) {
+            zofka.turnRight(45);
+            zofka.move(50);
+            zofka.turnRight(180);
+            zofka.move(50);
+            zofka.turnLeft(90);
+            zofka.move(50);
+            zofka.turnRight(180);
+            zofka.move(50);
+            zofka.turnRight(45);
+            zofka.move(80);
+        }
+
+
         //TODO tady bude kód
     }
 
